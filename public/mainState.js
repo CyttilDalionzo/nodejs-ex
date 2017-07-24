9a8400acf37b7f7ef4cb9743698549b52fcd8743
CyttilDalionzo <cyttildalionzo@gmail.com>
2017-07-24 20:26:43 +0200

    Bee game!
    Decided to make something simpler (instead of a full-on 2d shooter
    platformer).
    The game works, though it's not polished or finished at all.

--- public/mainState.js
+++ public/mainState.js
@@ -2,17 +2,26 @@ var Scene = {};
 
 var myNumber = -1;
 var myRoom = -1;
-var allPlayers = [];
-var myPlayer = null;
 
-var iAmTheBoss = false;
+var allPlayers = [];
+var ball = null;
+
 var iShouldSend = 0;
 var accumDeltaTime = 0;
+var waitingForPlayer = true;
 
 var globalSocket = null;
 
-var CELL_SIZE = 50;
-var MAP = null;
+var PLAYER_RADIUS = 3;
+var BALL_RADIUS = 1.5;
+
+var SERVER_WIDTH = 100
+var SERVER_HEIGHT = 40;
+var GAME_SCALE = 1;
+
+var buttons = [];
+
+
 
 Scene.Main = function (game) {
 
@@ -20,7 +29,7 @@ Scene.Main = function (game) {
 
 Scene.Main.prototype = {
 	preload: function() {
-		game.stage.backgroundColor = '#555';
+		game.stage.backgroundColor = '#a4c5f9';
 
 		game.load.image('player1', 'Player1.png');
 		game.load.image('player2', 'Player2.png');
@@ -32,6 +41,12 @@ Scene.Main.prototype = {
 	},
 
 	create: function () {
+		// RESIZING & RESCALING
+		game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+		game.scale.refresh();
+
+		GAME_SCALE = Math.min(game.width / SERVER_WIDTH, game.height / SERVER_HEIGHT);
+
 	    /*** 
 	     * WEBSOCKET STUFF
 	    ***/
@@ -39,51 +54,40 @@ Scene.Main.prototype = {
 
 	    // Retrieve room/game data
 	    globalSocket.on('data', function(msg) {
+
 	    	// Display some useful messages
 	    	myNumber = msg.player;
 	    	myRoom = msg.room;
 	    	$("#roomNum").html("You are in room " + myRoom + " as player " + myNumber);
 
-	    	// Determine who's boss
-	    	if(myNumber == 1) {
-	    		iAmTheBoss = true;
+	    	if(myNumber == 0) {
+	    		buttons = [Phaser.Keyboard.A, Phaser.Keyboard.S, Phaser.Keyboard.D];
+	    	} else if(myNumber == 1) {
+	    		buttons = [Phaser.Keyboard.D, Phaser.Keyboard.S, Phaser.Keyboard.A];
 	    	}
 
-	    	// Load the map
-	    	var map = msg.map;
-	    	for(var i = 0; i < map.length; i++) {
-	    		for(var j = 0; j < map[i].length; j++) {
-	    			Scene.Main.prototype.createTile(i, j, map[i][j]);
+	    	// Initialize game
+	    	var myGame = msg.game;
+
+	    	for(var i = 0; i < 2; i++) {
+	    		allPlayers[i] = [];
+	    		for(var j = 0; j < 3; j++) {
+	    			var serverPlayer = myGame.players[i][j];
+	    			allPlayers[i][j] = Scene.Main.prototype.createPlayer(serverPlayer.x, serverPlayer.y, i);
 	    		}
 	    	}
-	    	game.world.setBounds(0, 0, (map.length * CELL_SIZE), (map.length[0] * CELL_SIZE));
-	    	MAP = map;
 
-	    	// Create all players
-	    	var p = msg.players;
-	    	for(var i = 0; i < p.length; i++) {
-	    		var cP = p[i];
-	    		Scene.Main.prototype.createPlayer(cP.x, cP.y);
-	    	}
-
-	    	// Assign the right player to ourselves
-	    	myPlayer = allPlayers[(myNumber-1)];
-	    	game.camera.follow(myPlayer, Phaser.Camera.FOLLOW_PLATFORMER);
-	    });
-
-	    globalSocket.on('boss change', function(msg) {
-	    	if(msg == myNumber) {
-	    		iAmTheBoss = true;
-	    	}
+	    	ball = Scene.Main.prototype.createBall(myGame.ball.x, myGame.ball.y);
 	    });
 
 	    // Retrieve wait/start/stop signals
 	    globalSocket.on('signal', function(msg) {
 	    	var statusHTML = "";
 
-	    	if(msg == "join") {
-	    		statusHTML = "A new player joined!";
-	    		Scene.Main.prototype.createPlayer(0, 0);
+	    	if(msg == "start") {
+	    		statusHTML = "The games have begun!";
+	    		waitingForPlayer = false;
+				// game.world.setBounds(0, 0, (map.length * CELL_SIZE), (map.length[0] * CELL_SIZE));
 	    	} else if(msg == "disconnect") {
 	    		statusHTML = "A player disconnected!";
 	    	}
@@ -93,49 +97,61 @@ Scene.Main.prototype = {
 
 	    // Retrieve updated world state FROM server
 	    globalSocket.on('update', function(msg) {
+
 	    	// Go through all players (even our own), and update them
-	    	for(var i = 0; i < allPlayers.length; i++) {
-	    		var cP = msg.players[i];
-	    		allPlayers[i].position.setTo(cP.x, cP.y);
+	    	for(var i = 0; i < 2; i++) {
+	    		for(var j = 0; j < 3; j++) {
+	    			var cP = msg.players[i][j];
+	    			if(myNumber == 1) { 
+	    				cP.x = (SERVER_WIDTH - cP.x); 
+	    			}
+
+	    			allPlayers[i][j].position.setTo(cP.x * GAME_SCALE, cP.y * GAME_SCALE);
+	    		}
 	    	}
+
+	    	console.log(msg.ball.x + " || " + msg.ball.y);
+
+	    	// Update the ball
+	    	var ballX = msg.ball.x * GAME_SCALE
+	    	if(myNumber == 1) {
+	    		ballX = (SERVER_WIDTH - msg.ball.x) * GAME_SCALE;
+	    	}
+	    	ball.position.setTo(ballX, msg.ball.y * GAME_SCALE);
 	    });
 	},
 
 	update: function () {
+		if(waitingForPlayer) {
+			return;
+		}
+
 		var deltaTime = game.time.elapsed / 1000;
 		accumDeltaTime += deltaTime;
 
-		var movementUpdate = [0, 0];
+		var movementUpdate = [0, 0, 0];
 
-		// left/right movement
-		if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
-	    {
-	        movementUpdate[0] = -1;
-	    }
-	    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
-	    {
-	        movementUpdate[0] = 1;
-	    }
+		if(game.input.keyboard.isDown(buttons[0])) {
+			movementUpdate[0] = 1;
+		}
 
-	    // jump movement
-	    if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
-	    {
-	        movementUpdate[1] = -1;
-	    }
-	    /*else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
-	    {
-	        movementUpdate[1] = 1;
-	    }*/
+		if(game.input.keyboard.isDown(buttons[1])) {
+			movementUpdate[1] = 1;
+		}
+
+		if(game.input.keyboard.isDown(buttons[2])) {
+			movementUpdate[2] = 1;
+		}
 
 	    // Send input TO other players (iff something changed)
-	    if(movementUpdate[0] != 0 || movementUpdate[1] != 0) {
-	    	var inputData = { num: (myNumber - 1), room: myRoom, input: movementUpdate };
+	    if(movementUpdate[0] != 0 || movementUpdate[1] != 0 || movementUpdate[2] != 0) {
+	    	var inputData = { num: myNumber, room: myRoom, input: movementUpdate };
     		globalSocket.emit('input', inputData);
 	    }
 
 	    // If we're the first player, activate update loop on server each frame
-	    // Or, in fact, do it at a lower rate (like, 30 FPS)
-	    if(iAmTheBoss) {
+	    // Or, in fact, do it at a lower rate (like, 30 FPS at the moment)
+	    if(myNumber == 1) {
 	    	iShouldSend = (iShouldSend + 1) % 2;
 	    	if(iShouldSend == 0) {
 		    	globalSocket.emit('update', { room: myRoom, dt: accumDeltaTime });
@@ -144,18 +160,38 @@ Scene.Main.prototype = {
 	    }
 	},
 
-	createPlayer: function(x, y) {
-		var temp = game.add.sprite(x, y, 'player1');
-		temp.width = temp.height = CELL_SIZE;
+	createPlayer: function(x, y, team) {
+		// convert to this specific game
+		var teamString = "";
+		if(team == 0) {
+			teamString = 'player1';
+		} else {
+			teamString = 'player2';
+		}
+
+		x = x * GAME_SCALE;
+		y = y * GAME_SCALE;
+
+		if(myNumber == 1) {
+			x = (SERVER_WIDTH * GAME_SCALE - x);
+		}
+
+		var temp = game.add.sprite(x, y, teamString);
+		temp.width = temp.height = PLAYER_RADIUS * 2 * GAME_SCALE;
 		temp.anchor.setTo(0.5, 0.5);
-		allPlayers.push(temp);
+
+		// face the opponent's goal
+		if(team != myNumber) {
+			temp.width *= -1;
+		}
+
+		return temp;
 	},
 
-	createTile: function(i, j, what) {
-		if(what == 0) {
-			return;
-		}
-		var temp = game.add.sprite(i * CELL_SIZE, j * CELL_SIZE, 'player2');
-		temp.width = temp.height = CELL_SIZE;
+	createBall: function(x,y) {
+		var temp = game.add.sprite(x * GAME_SCALE, y * GAME_SCALE, "ball");
+		temp.width = temp.height = BALL_RADIUS * 2 * GAME_SCALE;
+		temp.anchor.setTo(0.5, 0.5);
+		return temp;
 	}
 }
--- server.js
+++ server.js
@@ -117,16 +117,18 @@ console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app ;
 
+// MY GAME CODE
+
 var roomID = -1;
 var amountInRoom = 0;
 var games = {};
 
-var MAX_PLAYER_AMOUNT = 4;
-var GRAVITY_CONST = 120;
+var GRAVITY_CONST = 1;
+var PLAYER_RADIUS = 3;
+var BALL_RADIUS = 1.5;
 
-var CELL_SIZE = 50;
-var MAP_WIDTH = 30;
-var MAP_HEIGHT = 20;
+var WIDTH = 100;
+var HEIGHT = 40;
 
 // A user connects
 io.on('connection', function(socket) {
@@ -141,39 +143,30 @@ io.on('connection', function(socket) {
     roomID++;
 
     // Create the game on the server
-    games[roomID] = { players: [], map: [], stats: [] };
-
-    // Create the game map, assign it
-    games[roomID].map = createMap();
+    games[roomID] = { players: [createPlayers(0), createPlayers(1)], ball: createBall(), score: [0, 0] };
   }
 
   var myRoom = roomID;
   var player = 0;
 
-  /*** 
-    * If there's already a game going on that has room, join it
-   ***/
-  if(amountInRoom < MAX_PLAYER_AMOUNT) {
+  if(amountInRoom == 1) {
+    player = 1;
+  }
 
-    // Tell all players that somebody has joined
-    io.in(roomID).emit("signal", "join");
+  socket.join(roomID);
 
-    // Add player to room count
+  // Return game data to player (only sender)
+  // Data includes: room number + player number + map + all other players 
+  var dataObject = { room: roomID, player: player, game: games[roomID] };
+  socket.emit('data', dataObject);
+
+  // If the second player has joined, start the game!
+  if(player == 1) {
+    amountInRoom = 0;
+    io.in(roomID).emit("signal", "start");
+  } else {
+    // increase amount of people in room
     amountInRoom++;
-
-    // Tell our user which player he is
-    player = amountInRoom;
-
-    // Join the room
-    socket.join(roomID);
-
-    // Join the game
-    createNewPlayer(roomID);
-
-    // Return game data to player (only sender)
-    // Data includes: room number + player number + map + all other players 
-    var dataObject = { room: roomID, player: player, map: games[roomID].map, players: games[roomID].players };
-    socket.emit('data', dataObject);
   }
 
   /*** 
@@ -192,230 +185,123 @@ io.on('connection', function(socket) {
     var curGame = games[myRoom];
     var players = curGame.players;
 
-    // TO DO
-    // Actually run game code and update the world
+    // UPDATE PLAYERS
+    for(var i = 0; i < 2; i++) {
+      for(var j = 0; j < 3; j++) {
+        var p = players[i][j];
 
-    // For all players ...
-    for(var i = 0; i < players.length; i++) {
-      var cp = players[i];
+        // GRAVITY
+        p.velY += GRAVITY_CONST;
 
-      // ignore players who have quit
-      if(cp == null) {
-        continue;
-      }
-
-      // process input
-      if(cp.desiredInput != null) {
-        var ip = cp.desiredInput;
-        if(ip[0] != 0) {
-          cp.velX = ip[0] * cp.speed;
+        // INPUT
+        if(p.desiredInput == 1) {
+          p.velY -= GRAVITY_CONST * 10;
         }
 
-        if(ip[1] == -1) {
-          cp.velY = ip[1] * cp.speed;
+        p.desiredInput = 0;
+
+        // ACTUALLY MOVING
+        p.y += p.velY * dt;
+
+        // USE TOP AND BOTTOM BOUNDS
+        if(p.y > HEIGHT - PLAYER_RADIUS) {
+          p.velY *= -0.5;
+          p.y = HEIGHT - PLAYER_RADIUS;
+        } else if(p.y < 0.0 + PLAYER_RADIUS) {
+          p.velY *= -0.5;
+          p.y = 0.0 + PLAYER_RADIUS;
         }
-        cp.desiredInput = null;
-      }
-
-      // apply gravity
-      cp.velY += GRAVITY_CONST * dt;
-
-      // CHECK FOR COLLISIONS
-      // essentially, we calculate a square of grid positions that could possibly have been hit
-
-      var remainingtime = 1;
-      //var epsilon = 0.000001;
-      var touching = false;
-
-      while(remainingtime > 0) {
-        var hitSomething = false;
-
-        var startGrids = worldToGrid(cp.x, cp.y);
-        var endGrids = worldToGrid(cp.x + cp.velX * dt, cp.y + cp.velY * dt); 
-        var bounds = [ [Math.min(startGrids[0], endGrids[0]) - 1, Math.max(startGrids[0], endGrids[0]) + 1 ], [Math.min(startGrids[1], endGrids[1]) - 1, Math.max(startGrids[1], endGrids[1]) + 1] ];
-
-        // start and end coordinates
-        var nearestHit = 1, nearestHitNormals = [];
-
-        var newVelocityX = cp.velX;
-        var newVelocityY = cp.velY;
-
-        if(cp.velX == 0 && cp.velY == 0) {
-
-        }
-
-        if(!touching) {
-          if(cp.velX == 0) {
-            newVelocityX = 0.00001;
-          } 
-          
-          if(cp.velY == 0) {
-            newVelocityY = 0.00001;
-          }
-        }
-
-        var startC = [cp.x, cp.y];
-        var endC = [cp.x + newVelocityX * dt, cp.y + newVelocityY * dt];
-
-        // go through this square of squares, and calculate hit positions
-        for(var i = bounds[0][0]; i <= bounds[0][1]; i++) {
-          for(var j = bounds[1][0]; j <= bounds[1][1]; j++) {
-
-            // if there's no square here, continue
-            if(i < 0 || j < 0 || i >= MAP_WIDTH || j >= MAP_HEIGHT || curGame.map[i][j] == 0) {
-              continue;
-            }
-
-            // pad the square with the size of our player
-            var square = { x: (i + 0.5) * CELL_SIZE, y: (j + 0.5) * CELL_SIZE, halfWidth: CELL_SIZE * 0.5, halfHeight: CELL_SIZE * 0.5};
-
-            var paddingX = CELL_SIZE * 0.5;
-            var paddingY = CELL_SIZE * 0.5;
-            var deltaX = (endC[0] - startC[0]);
-            var deltaY = (endC[1] - startC[1]);
-
-            // test the movement segment against this padded square
-            var scaleX = 1.0 / deltaX;
-            var scaleY = 1.0 / deltaY;
-
-            var signX = (scaleX > 0) ? 1 : -1;
-            var signY = (scaleY > 0) ? 1 : -1;
-
-            var nearTimeX = 0, farTimeX = 0, nearTimeY = 0, farTimeY = 0;
-
-            // find X hit time
-            nearTimeX = (square.x - signX * (square.halfWidth + paddingX) - startC[0]) * scaleX;
-            farTimeX = (square.x + signX * (square.halfWidth + paddingX) - startC[0]) * scaleX;
-
-            // find Y hit time
-            nearTimeY = (square.y - signY * (square.halfHeight + paddingY) - startC[1]) * scaleY;
-            farTimeY = (square.y + signY * (square.halfHeight + paddingY) - startC[1]) * scaleY;
-
-            // if no hit at all, ignore and move on
-            if(nearTimeX > farTimeY || nearTimeY > farTimeX) {
-              continue;
-            }
-
-            var nearTime = Math.max(nearTimeX, nearTimeY);
-            var farTime = Math.min(farTimeX, farTimeY);
-
-            // also can't be a hit
-            if(nearTime >= 1 || farTime <= 0) {
-              continue;
-            }
-
-            hitSomething = true;
-
-            // calculate hit time
-            var hitTime = Math.min(Math.max(nearTime, 0), 1);
-
-            // if hit, check if it's the nearest hit
-            if(hitTime < nearestHit) {
-              nearestHit = hitTime;
-
-              var hitNormals = [0, -signY];
-              if(nearTimeX > nearTimeY) {
-                hitNormals = [-signX, 0];
-              }
-              nearestHitNormals = hitNormals;
-            }
-          }
-        }
-
-        // add speed to position (according to how long the thing can keep moving until it collides)
-        if(hitSomething && nearestHit > remainingtime) {
-          nearestHit = remainingtime;
-          hitSomething = false;
-        }
-
-        console.log(newVelocityX + " || " + newVelocityY);
-
-        cp.x += newVelocityX * nearestHit * dt;
-        cp.y += newVelocityY * nearestHit * dt;
-
-        remainingtime = remainingtime - nearestHit;
-
-        console.log(hitSomething + " || " + nearestHitNormals);
-
-        // if we hit something, slide it along!
-        if(hitSomething) {
-          touching = true;
-          if(nearestHitNormals[0] == 0) {
-            cp.velY = 0;
-          }
-
-          if(nearestHitNormals[1] == 0) {
-            cp.velX = 0;
-          }
-        }
-      }
-
-      // reset x-speed
-      cp.velX *= 0.94;
-      if(cp.velX < 0.1) {
-        cp.velX = 0;
       }
     }
 
+    // UPDATE BALL
+    var ball = curGame.ball;
 
-    // TO DO
-    // It's not necessary to send the whole game, so cherry-pick
-    // For example, the map should be sent once and then forgotten about
-    var updatedGame = { players: players };
+    ball.velY += GRAVITY_CONST;
+
+    ball.y += ball.velY * dt;
+    ball.x += ball.velX * dt;
+
+    // BALL UPPER AND LOWER BOUNDS
+    if(ball.y > HEIGHT - BALL_RADIUS) {
+      ball.velY *= -1.1;
+      ball.y = HEIGHT - BALL_RADIUS;
+    } else if(ball.y < 0.0 + BALL_RADIUS) {
+      ball.velY = 0;
+      ball.y = 0.0 + BALL_RADIUS;
+    }
+
+    // BALL LEFT AND RIGHT BOUNDS ( = SCORING)
+    if(ball.x < 0.0) {
+      ball.x = 0.5 * WIDTH;
+      ball.y = 0.5 * HEIGHT;
+      ball.velY = -20;
+      ball.velX = -10;
+      curGame.score[1]++;
+    } else if(ball.x > WIDTH) {
+      ball.x = 0.5 * WIDTH;
+      ball.y = 0.5 * HEIGHT;
+      ball.velY = -20;
+      ball.velX = 10;
+      curGame.score[0]++;
+    }
+
+    var foundCollision = false;
+    for(var i = 0; i < 2; i++) {
+      for(var j = 0; j < 3; j++) {
+        var p = players[i][j];
+
+        // we "pretend" the player is a static circle, and the ball dynamic
+        var depth = (BALL_RADIUS + PLAYER_RADIUS) - Math.sqrt(Math.pow(p.x - ball.x, 2) + Math.pow(p.y - ball.y, 2));
+        if(depth >= 0) {
+          foundCollision = true;
+
+          var norm = depth + (BALL_RADIUS + PLAYER_RADIUS);
+          var direction = [(ball.x - p.x) / norm, (ball.y - p.y) / norm];
+
+          ball.x += direction[0] * depth;
+          ball.y += direction[1] * depth;
+
+          var BALL_SPEED = Math.sqrt(Math.pow(ball.velX, 2) + Math.pow(ball.velY, 2));
+          var norm2 = Math.sqrt(Math.pow(p.x, 2) + Math.pow(p.y, 2));
+          direction[0] += p.velX / norm2;
+          direction[1] += p.velY / norm2;
+
+          ball.velX = direction[0] * BALL_SPEED;
+          ball.velY = direction[1] * BALL_SPEED;
+
+          break;
+        }
+      }
+
+      if(foundCollision) {
+        break;
+      }
+    }
 
     // The new game state is relayed as an update to all players
-    io.in(myRoom).emit('update', updatedGame);
+    io.in(myRoom).emit('update', curGame);
+
   });
 
   // When something happens, input is send from player to server
   socket.on('input', function(msg) {
+
       // Shortcut to this player's room
       myRoom = msg.room;
-      var curGame = games[myRoom];
-      var myPlayer = curGame.players[msg.num];
-      myPlayer.desiredInput = msg.input
+      var myPlayers = games[myRoom].players[msg.num];
 
-      var input = msg.input;
-
-      // Update the velocity of this player (and thus its position)
-      if(input[1] == -1) {
-         myPlayer.velY = input[1] * myPlayer.speed;
-      } else {
-         myPlayer.velX = input[0] * myPlayer.speed;
+      // Update desired input for my players
+      for(var i = 0; i < myPlayers.length; i++) {
+        myPlayers[i].desiredInput = msg.input[i];
       }
-
-
-      // TO DO
-      // Update the state of this player (alive, shooting, etc.)
   });
 
   // And, of course, disconnect
   socket.on('disconnect', function() {
-    var curGame = games[myRoom];
 
-    // Tell the other players you've quit
+    // tell the other player you've quit
     socket.broadcast.to(myRoom).emit('signal', "disconnect");
-
-    if(player == 0) {
-      // If we are the first player...
-      if(curGame.players.length < 2) {
-        // If there's nobody else, simply delete the room
-        curGame = {};
-      } else {
-        // If there are other players, find another one to control the update loop
-        curGame.players[player] = null;
-
-        for(var i = 0; i < curGame.players.length; i++) {
-          if(curGame.players[i] != null) {
-            io.in(myRoom).emit('boss change', i);
-          }
-        }
-      }
-
-    } else {
-      // Simply delete yourself from the game
-      curGame.players[player] = null;
-    }
 
     console.log('user disconnected');
   });
@@ -425,33 +311,23 @@ server.listen(8080, function(){
   console.log('listening on *:8080');
 });
 
-function createNewPlayer(id) {
-  var newPlayer = { x: 100, y: 100, velX: 0, velY: 0, lives: 3, health: 100, speed: 40, desiredInput: null };
-  games[id].players.push(newPlayer);
-}
+function createPlayers(playerNum) {
+  var positionArr = [0.1, 0.4, 0.75, 0.25, 0.6, 0.9];
 
-function createMap() {
-  var map = [];
-
-  for(var i = 0; i < MAP_WIDTH; i++) {
-    map[i] = [];
-    for(var j = 0; j < MAP_HEIGHT; j++) {
-      // create border
-      if(i == 0 || i == MAP_WIDTH-1 || j == 0 || j == MAP_HEIGHT-1 || (j%4 == 0 && i%2 == 0)) {
-        map[i][j] = 1;
-      } else {
-        map[i][j] = 0;
-      }
-    }
+  var arr = [];
+  for(var i = 0; i < 3; i++) {
+    arr[i] = createPlayer(WIDTH * positionArr[(i + playerNum * 3)]);
   }
-
-  return map;
+  return arr;
 }
 
-function worldToGrid(x, y) {
-  x = Math.floor(x / CELL_SIZE);
-  y = Math.floor(y / CELL_SIZE);
-  return [x, y];
+function createPlayer(xPos) {
+  var newPlayer = { x: xPos, y: HEIGHT * 0.5, velX: 0, velY: 0, desiredInput: 0 };
+  return newPlayer;
+}
+
+function createBall() {
+  return { x: WIDTH * 0.5, y: HEIGHT * 0.5, velX: 10, velY: -20 };
 }
 
 function intersect(rect1, rect2) {
@@ -461,3 +337,26 @@ function intersect(rect1, rect2) {
          rect1.height + rect1.y > rect2.y;
 }
 
+function closestpointonline(lx1, ly1, lx2, ly2, x0, y0){ 
+     var A1 = ly2 - ly1; 
+     var B1 = lx1 - lx2; 
+     var C1 = (ly2 - ly1)*lx1 + (lx1 - lx2)*ly1; 
+     var C2 = -B1*x0 + A1*y0; 
+     var det = A1*A1 - -B1*B1; 
+     var cx = 0; 
+     var cy = 0; 
+     
+     if(det != 0) { 
+      cx = ((A1*C1 - B1*C2) / det); 
+      cy = ((A1*C2 - -B1*C1) / det); 
+     } else { 
+      cx = x0; 
+      cy = y0; 
+     } 
+
+     return [cx, cy];
+}
+
+function clamp(low, val, high) {
+  return Math.max(Math.min(val, high), low);
+}
